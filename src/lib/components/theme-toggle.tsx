8452868aeaf1cae852207506d284478a9bd3b5d7
Agustinus Nathaniel <berserk.nate@gmail.com>
2026-07-20 22:27:46 +0700

    chore: up-latest

--- src/lib/components/theme-toggle.tsx
+++ src/lib/components/theme-toggle.tsx
@@ -1,24 +1,14 @@
 import { Classic } from '@theme-toggles/react';
 import { useTheme } from 'next-themes';
-import { type SetStateAction, useCallback } from 'react';
-import '@theme-toggles/react/css/Classic.css';
+import { useCallback } from 'react';
+import '@theme-toggles/react/styles/classic.css';
 
 export function ThemeToggle() {
   const { setTheme, resolvedTheme } = useTheme();
 
-  const handleToggle = useCallback(
-    (light: SetStateAction<boolean>) => setTheme(light ? 'light' : 'dark'),
-    [setTheme]
-  );
+  const toggleTheme = useCallback(() => {
+    setTheme(resolvedTheme === 'dark' ? 'light' : 'dark');
+  }, [setTheme, resolvedTheme]);
 
-  return (
-    <Classic
-      className="text-3xl"
-      onPointerEnterCapture={undefined}
-      onPointerLeaveCapture={undefined}
-      placeholder=""
-      toggle={handleToggle}
-      toggled={resolvedTheme === 'light'}
-    />
-  );
+  return <Classic className="text-3xl" onClick={toggleTheme} />;
 }
