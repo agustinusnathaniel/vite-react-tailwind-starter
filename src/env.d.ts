9143bcf6b63ed2d48f5c98bb8101617bed6cead9
Agustinus Nathaniel <berserk.nate@gmail.com>
2026-09-14 19:13:42 +0700

    chore: up-latest

--- src/env.d.ts
+++ src/env.d.ts
@@ -4,8 +4,7 @@ type ImportMetaEnvAugmented =
   // eslint-disable-next-line @typescript-eslint/consistent-type-imports
   import('@julr/vite-plugin-validate-env').ImportMetaEnvAugmented<
     // eslint-disable-next-line @typescript-eslint/consistent-type-imports
-    typeof import('../env').default
-  >;
+    typeof import('../env').default>;
 
 // eslint-disable-next-line @typescript-eslint/no-empty-object-type
 interface ImportMetaEnv extends ImportMetaEnvAugmented {}
